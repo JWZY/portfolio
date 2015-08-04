17ef65eefa596fdd9c7fa77762cd18bd5c55c2ca
JWZY <psyjavan@gmail.com>
2015-08-04 09:28:17 -0400

    commits

--- examples/examples.js
+++ examples/examples.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
-		sectionsColor: ['white', 'white0', 'white', 'white'],
 		anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage'],
 		menu: '#menu',
 		scrollingSpeed: 1000,
