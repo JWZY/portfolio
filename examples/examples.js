37bc474ee5b05aaa25e17dae5d63b15ef2d4b525
Javan Wang <javan@roadmunk.com>
2015-08-11 13:28:26 -0400

    wow holy ship it actually worked

--- examples/examples.js
+++ examples/examples.js
@@ -4,7 +4,19 @@ $(document).ready(function() {
 		menu: '#menu',
 		scrollingSpeed: 1000,
 		slidesNavigation: true,
-		autoScrolling: false
-	});
+		autoScrolling: false,
+		scrollOverflow: true,
 
+	    // Hide the slides container before the next slide loads
+	    onSlideLeave: function(anchorLink, index, slideIndex, direction) {
+	        $.fn.fullpage.setScrollingSpeed(0);
+	        $('.fp-section').find('.fp-slidesContainer').hide();
+	    },
+	  
+	    // Display the slides container by fading it in after the next slide has been loaded.
+	    afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {
+	        $('.fp-section').find('.fp-slidesContainer').fadeIn(400);
+	        $.fn.fullpage.setScrollingSpeed(1000);
+	    }
+	});
 });
--- jquery.fullPage.js
+++ jquery.fullPage.js
@@ -191,7 +191,7 @@
 
             }else{
                 $htmlBody.css({
-                    'overflow' : 'visible',
+                    'overflow' : 'hidden',
                     'height' : 'initial'
                 });
 
