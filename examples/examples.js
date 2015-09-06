33ecdf283f28a96ab6f1411c467c6cc316268ac7
JWZY <psyjavan@gmail.com>
2015-09-06 18:44:01 -0400

    massive styling and content update

--- examples/examples.js
+++ examples/examples.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
-		anchors: ['home', 'about', 'portfolio', 'contact'],
+		anchors: ['portfolio', 'about', 'contact'],
 		menu: '#menu',
 		scrollingSpeed: 1000,
 		slidesNavigation: true,
-		autoScrolling: false,
+		autoScrolling: false
 
 	    // Hide the slides container before the next slide loads
-	    onSlideLeave: function(anchorLink, index, slideIndex, direction) {
-	        $.fn.fullpage.setScrollingSpeed(0);
-	        $('.fp-section').find('.fp-slidesContainer').hide();
-	    },
+	    //onSlideLeave: function(anchorLink, index, slideIndex, direction) {
+	    //    $.fn.fullpage.setScrollingSpeed(0);
+	    //    $('.fp-section').find('.fp-slidesContainer').hide();
+	    //},
 	  
 	    // Display the slides container by fading it in after the next slide has been loaded.
-	    afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {
-	        $('.fp-section').find('.fp-slidesContainer').fadeIn(400);
-	        $.fn.fullpage.setScrollingSpeed(1000);
-	    },
+	    //afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {
+	    //    $('.fp-section').find('.fp-slidesContainer').fadeIn(400);
+	    //    $.fn.fullpage.setScrollingSpeed(1000);
+	    //},
 	});
 });
 
